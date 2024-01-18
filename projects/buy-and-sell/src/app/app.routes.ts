1c55dcc1a17bf855c419c6da87c532d98b673187
Mohammed Insafp(UST <233659@ust-global.com>
2024-01-18 18:08:45 +0530

    first commit

--- projects/buy-and-sell/src/app/app.routes.ts
+++ projects/buy-and-sell/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDetailsPageComponent } from './listing-details-page/listing-details-page.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
 export const routes: Routes = [
-    {path:'listings', component:ListingsPageComponent,pathMatch:'full'}
-    
+    {path:'listings', component:ListingsPageComponent,pathMatch:'full'},
+    {path:'listings/:id',component:ListingDetailsPageComponent},
+    {path:'contact/:id',component:ContactPageComponent},
+    {path:'edit-listing/:id',component:EditListingPageComponent},
+    {path:'my-listings',component:MyListingsPageComponent,
+    {path:'new-listing',component:NewListingPageComponent}
+
 ];
---
+++ projects/buy-and-sell/src/app/listing-details-page/listing-details-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-details-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './listing-details-page.component.html',
+  styleUrl: './listing-details-page.component.css'
+})
+export class ListingDetailsPageComponent {
+
+}
