553d26e16a266e33cd8c9db9a0bce0fda429bbaa
Mohammed Insafp(UST <233659@ust-global.com>
2024-01-19 10:11:14 +0530

    first commit

--- projects/buy-and-sell/src/app/listings-page/listings-page.component.ts
+++ projects/buy-and-sell/src/app/listings-page/listings-page.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { fakeListings } from '../fake-data';
 import { Listing } from '../types';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    // ... other imports
+  ],
+  // ... other configurations
+})
 
 @Component({
   selector: 'app-listings-page',
